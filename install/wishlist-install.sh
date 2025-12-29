b2427685e6d9a3d58f4aba24b64363577286ecd2
Marc Went <marc@went.io>
2025-12-29 14:56:05 +0100

    try something

--- install/wishlist-install.sh
+++ install/wishlist-install.sh
@@ -20,8 +20,7 @@ msg_ok "Installed dependencies"
 NODE_VERSION="24" NODE_MODULE="pnpm" setup_nodejs
 fetch_and_deploy_gh_release "wishlist" "cmintey/wishlist" "tarball"
 LATEST_APP_VERSION=$(get_latest_github_release "cmintey/wishlist")
-VERSION="${LATEST_APP_VERSION}" 
-SHA="${LATEST_APP_VERSION}" 
+
 msg_info "Installing Wishlist"
 cd /opt/wishlist || exit
 cat <<EOF >/opt/wishlist/.env
@@ -36,7 +35,7 @@ $STD pnpm install
 $STD pnpm svelte-kit sync
 $STD pnpm prisma generate
 $STD sed -i 's|/usr/src/app/|/opt/wishlist/|g' $(grep -rl '/usr/src/app/' /opt/wishlist)
-$STD pnpm run build
+VERSION="${LATEST_APP_VERSION}" SHA="${LATEST_APP_VERSION}" $STD pnpm run build
 $STD pnpm prune --prod
 $STD chmod +x /opt/wishlist/entrypoint.sh
 msg_ok "Installed Wishlist"
